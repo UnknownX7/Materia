1056ffeccc1278595f0ea5bfeb75d309faa65954
UnknownX7 <UnknownX7@users.noreply.github.com>
2024-02-09 13:04:25 -0600

    Add WorkManager

---
+++ Materia/Game/WorkManager.cs
@@ -0,0 +1,6 @@
+namespace Materia.Game;
+
+public static unsafe class WorkManager
+{
+    public static ECGen.Generated.Command.Work.WorkManager* NativePtr { get; } = ((ECGen.Generated.Command.Work.WorkManager.StaticFields*)Il2CppType<ECGen.Generated.Command.Work.WorkManager>.Instance.NativePtr->staticFields)->instance;
+}
