466fbdbe2cdd8d53b8fd0452aa8260da097cee4f
UnknownX7 <UnknownX7@users.noreply.github.com>
2024-06-05 03:22:58 -0500

    Add AssetManager

---
+++ Materia/Game/AssetManager.cs
@@ -0,0 +1,8 @@
+namespace Materia.Game;
+
+public static unsafe class AssetManager
+{
+    public static ECGen.Generated.Command.AssetManager.StaticFields* NativePtr { get; } = (ECGen.Generated.Command.AssetManager.StaticFields*)Il2CppType<ECGen.Generated.Command.AssetManager>.Instance.NativePtr->staticFields;
+
+
+}
